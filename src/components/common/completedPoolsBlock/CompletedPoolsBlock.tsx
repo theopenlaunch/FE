f5a7c29e885325b7b51e1571a5b0a11c7be5c5da
sepezho <sepezho@gmail.com>
2022-05-30 18:23:21 +0200

    merge fixes

--- src/components/common/completedPoolsBlock/CompletedPoolsBlock.tsx
+++ src/components/common/completedPoolsBlock/CompletedPoolsBlock.tsx
@@ -7,25 +7,25 @@ import * as Styled from "./CompletedPoolsBlock.Styled";
 import mask from "../../../images/mask.png";
 
 const PoolsBlock = () => {
-  return (
-    <Styled.Container>
-      <Styled.Content>
-        <Styled.Header>
-          <Heading02 id="upcoming_pools">Completed Pools</Heading02>
-          <Styled.Filters>
-            <Styled.Filter style={{ backgroundColor: "#A259FF" }}>
-              Public
-            </Styled.Filter>
-            <Styled.Filter style={{ backgroundColor: "#FC9114" }}>
-              Private
-            </Styled.Filter>
-            <Styled.Filter style={{ backgroundColor: "#0FA958" }}>
-              87 - 100 %
-            </Styled.Filter>
-          </Styled.Filters>
-        </Styled.Header>
-        <Styled.Pools>
-          <Styled.Pool>
+	return (
+		<Styled.Container>
+			<Styled.Content>
+				<Styled.Header>
+					<Heading02 id="upcoming_pools">Completed Pools</Heading02>
+					<Styled.Filters>
+						<Styled.Filter style={{ backgroundColor: "#A259FF" }}>
+							Public
+						</Styled.Filter>
+						<Styled.Filter style={{ backgroundColor: "#FC9114" }}>
+							Private
+						</Styled.Filter>
+						<Styled.Filter style={{ backgroundColor: "#0FA958" }}>
+							87 - 100 %
+						</Styled.Filter>
+					</Styled.Filters>
+				</Styled.Header>
+				<Styled.Pools>
+					{/* <Styled.Pool>
             <Styled.TopContent>
               <Styled.ImageWrapper>
                 <Styled.PoolImage src={mask} alt="Mask" />
@@ -69,152 +69,17 @@ const PoolsBlock = () => {
                 <Styled.BarDataBLC>237499.48/ 239499.48 BLC</Styled.BarDataBLC>
               </Styled.BarDataContent>
             </Styled.BottomContent>
-          </Styled.Pool>
-          <Styled.Pool>
-            <Styled.TopContent>
-              <Styled.ImageWrapper>
-                <Styled.PoolImage src={mask} alt="Mask" />
-              </Styled.ImageWrapper>
-              <Styled.TextContent>
-                <Styled.Visibility>
-                  <Styled.Tag style={{ backgroundColor: "#A259FF" }}>
-                    Public
-                  </Styled.Tag>
-                </Styled.Visibility>
-                <Styled.PoolHeader>
-                  <Styled.PoolHeading>Ecovid Mask</Styled.PoolHeading>
-                  <Styled.Tag style={{ backgroundColor: "#FC9114" }}>
-                    1 BUSD = 518,55 BLC
-                  </Styled.Tag>
-                </Styled.PoolHeader>
-                <Styled.PoolText>
-                  The startup team is working on a technology for recyclable
-                  protective masks based on natural nanomaterials.
-                </Styled.PoolText>
-              </Styled.TextContent>
-            </Styled.TopContent>
-            <Styled.BottomContent>
-              <Styled.RaiseAmountContent>
-                <Styled.RaiseAmount>
-                  <Styled.RaiseText>Total Raise</Styled.RaiseText>
-                  <Styled.RaiseHeading>237499.48 BUSD</Styled.RaiseHeading>
-                </Styled.RaiseAmount>
-                <Styled.RaiseAmount>
-                  <Styled.RaiseText>Maximum</Styled.RaiseText>
-                  <Styled.RaiseHeadingSmall>
-                    239499.48 BUSD
-                  </Styled.RaiseHeadingSmall>
-                </Styled.RaiseAmount>
-              </Styled.RaiseAmountContent>
-              <Styled.ProgressBarWrapper>
-                <ProgressBar percent={74.8} />
-              </Styled.ProgressBarWrapper>
-              <Styled.BarDataContent>
-                <Styled.BarDataText>74.8%</Styled.BarDataText>
-                <Styled.BarDataBLC>237499.48/ 239499.48 BLC</Styled.BarDataBLC>
-              </Styled.BarDataContent>
-            </Styled.BottomContent>
-          </Styled.Pool>
-          <Styled.Pool>
-            <Styled.TopContent>
-              <Styled.ImageWrapper>
-                <Styled.PoolImage src={mask} alt="Mask" />
-              </Styled.ImageWrapper>
-              <Styled.TextContent>
-                <Styled.Visibility>
-                  <Styled.Tag style={{ backgroundColor: "#A259FF" }}>
-                    Public
-                  </Styled.Tag>
-                </Styled.Visibility>
-                <Styled.PoolHeader>
-                  <Styled.PoolHeading>Ecovid Mask</Styled.PoolHeading>
-                  <Styled.Tag style={{ backgroundColor: "#FC9114" }}>
-                    1 BUSD = 518,55 BLC
-                  </Styled.Tag>
-                </Styled.PoolHeader>
-                <Styled.PoolText>
-                  The startup team is working on a technology for recyclable
-                  protective masks based on natural nanomaterials.
-                </Styled.PoolText>
-              </Styled.TextContent>
-            </Styled.TopContent>
-            <Styled.BottomContent>
-              <Styled.RaiseAmountContent>
-                <Styled.RaiseAmount>
-                  <Styled.RaiseText>Total Raise</Styled.RaiseText>
-                  <Styled.RaiseHeading>237499.48 BUSD</Styled.RaiseHeading>
-                </Styled.RaiseAmount>
-                <Styled.RaiseAmount>
-                  <Styled.RaiseText>Maximum</Styled.RaiseText>
-                  <Styled.RaiseHeadingSmall>
-                    239499.48 BUSD
-                  </Styled.RaiseHeadingSmall>
-                </Styled.RaiseAmount>
-              </Styled.RaiseAmountContent>
-              <Styled.ProgressBarWrapper>
-                <ProgressBar percent={10} />
-              </Styled.ProgressBarWrapper>
-              <Styled.BarDataContent>
-                <Styled.BarDataText>10%</Styled.BarDataText>
-                <Styled.BarDataBLC>237499.48/ 239499.48 BLC</Styled.BarDataBLC>
-              </Styled.BarDataContent>
-            </Styled.BottomContent>
-          </Styled.Pool>
-          <Styled.Pool>
-            <Styled.TopContent>
-              <Styled.ImageWrapper>
-                <Styled.PoolImage src={mask} alt="Mask" />
-              </Styled.ImageWrapper>
-              <Styled.TextContent>
-                <Styled.Visibility>
-                  <Styled.Tag style={{ backgroundColor: "#A259FF" }}>
-                    Public
-                  </Styled.Tag>
-                </Styled.Visibility>
-                <Styled.PoolHeader>
-                  <Styled.PoolHeading>Ecovid Mask</Styled.PoolHeading>
-                  <Styled.Tag style={{ backgroundColor: "#FC9114" }}>
-                    1 BUSD = 518,55 BLC
-                  </Styled.Tag>
-                </Styled.PoolHeader>
-                <Styled.PoolText>
-                  The startup team is working on a technology for recyclable
-                  protective masks based on natural nanomaterials.
-                </Styled.PoolText>
-              </Styled.TextContent>
-            </Styled.TopContent>
-            <Styled.BottomContent>
-              <Styled.RaiseAmountContent>
-                <Styled.RaiseAmount>
-                  <Styled.RaiseText>Total Raise</Styled.RaiseText>
-                  <Styled.RaiseHeading>237499.48 BUSD</Styled.RaiseHeading>
-                </Styled.RaiseAmount>
-                <Styled.RaiseAmount>
-                  <Styled.RaiseText>Maximum</Styled.RaiseText>
-                  <Styled.RaiseHeadingSmall>
-                    239499.48 BUSD
-                  </Styled.RaiseHeadingSmall>
-                </Styled.RaiseAmount>
-              </Styled.RaiseAmountContent>
-              <Styled.ProgressBarWrapper>
-                <ProgressBar percent={42} />
-              </Styled.ProgressBarWrapper>
-              <Styled.BarDataContent>
-                <Styled.BarDataText>42%</Styled.BarDataText>
-                <Styled.BarDataBLC>237499.48/ 239499.48 BLC</Styled.BarDataBLC>
-              </Styled.BarDataContent>
-            </Styled.BottomContent>
-          </Styled.Pool>
-        </Styled.Pools>
-        <Styled.ShowMore href="#completed_pools">
-          <p>Show More</p>
-          <Styled.IconWrapper>
-            <ChevronRightIcon />
-          </Styled.IconWrapper>
-        </Styled.ShowMore>
-      </Styled.Content>
-    </Styled.Container>
-  );
+          </Styled.Pool> */}
+				</Styled.Pools>
+				<Styled.ShowMore href="#completed_pools">
+					<p>Show More</p>
+					<Styled.IconWrapper>
+						<ChevronRightIcon />
+					</Styled.IconWrapper>
+				</Styled.ShowMore>
+			</Styled.Content>
+		</Styled.Container>
+	);
 };
 
 export default PoolsBlock;
--- src/components/common/upcomingPoolsBlock/UpcomingPoolsBlock.tsx
+++ src/components/common/upcomingPoolsBlock/UpcomingPoolsBlock.tsx
@@ -4,94 +4,118 @@ import { ChevronRightIcon } from "@heroicons/react/solid";
 import * as Styled from "./UpcomingPoolsBlock.Styled";
 import * as dayjs from "dayjs";
 import GetStatus from "../../../logic/GetStatus";
+import Tilt from "../../../logic/Tilt";
+import ModalInvoice from "../../ModalInvoice";
 
 var relativeTime = require("dayjs/plugin/relativeTime");
 dayjs.extend(relativeTime);
 
 const UpcomingPoolsBlock = () => {
-  const [bigArr, setBigArr] = useState({ status: [] });
+	const [bigArr, setBigArr] = useState({ status: [] });
+	const [isVisible, toggleModal] = useState(false);
+	const [projectData, setProjectData] = useState({});
 
-  useEffect(() => {
-    (async () => {
-      setBigArr(await GetStatus());
-    })();
-  }, []);
+	useEffect(() => {
+		(async () => {
+			setBigArr(await GetStatus());
+		})();
+	}, []);
 
-  return (
-    <Styled.Container>
-      <Styled.Content>
-        <Styled.Header>
-          <Heading02 id="upcoming_pools">Upcoming Pools</Heading02>
-          <Styled.Filters>
-            <Styled.Filter style={{ backgroundColor: "#A259FF" }}>
-              Public
-            </Styled.Filter>
-            <Styled.Filter style={{ border: "1px solid #000", color: "#000" }}>
-              Private
-            </Styled.Filter>
-            <Styled.Filter style={{ backgroundColor: "#FF004C" }}>
-              0 - 5 days
-            </Styled.Filter>
-          </Styled.Filters>
-        </Styled.Header>
-        <Styled.Pools>
-          {bigArr.status.map((e: any) =>
-            e ? (
-              <Styled.Pool>
-                <Styled.ImageWrapper>
-                  <Styled.PoolImage src={e.Picture} alt="Eclipse" />
-                </Styled.ImageWrapper>
-                <Styled.TextContent>
-                  <Styled.PoolHeader>
-                    <Styled.PoolHeading>{e.Title}</Styled.PoolHeading>
-                    <Styled.Tags>
-                      <Styled.Tag style={{ backgroundColor: "#A259FF" }}>
-                        Public
-                      </Styled.Tag>
-                      <Styled.Tag style={{ backgroundColor: "#FF004D" }}>
-                        {e.EndDate === 10
-                          ? "none"
-                          : //@ts-ignore
-                            dayjs().to(dayjs(Number(e.EndDate)))}
-                        {/* dayjs(Number(e.EndDate)).format('MM/YYYY') */}
-                      </Styled.Tag>
-                    </Styled.Tags>
-                  </Styled.PoolHeader>
-                  <Styled.PoolText>
-                    {e.Description.length > 200
-                      ? e.Description.slice(0, 200) + "..."
-                      : e.Description}
-                  </Styled.PoolText>
-                  <Styled.Allocation>
-                    <Styled.AllocationAmount>
-                      <Styled.AllocationText>Rised (TON)</Styled.AllocationText>
-                      <Styled.AllocationHeading>
-                        {e.Rised}
-                      </Styled.AllocationHeading>
-                    </Styled.AllocationAmount>
-                    <Styled.AllocationAmount>
-                      <Styled.AllocationText>
-                        Maximum (TON)
-                      </Styled.AllocationText>
-                      <Styled.AllocationHeading>
-                        {e.Max}
-                      </Styled.AllocationHeading>
-                    </Styled.AllocationAmount>
-                  </Styled.Allocation>
-                </Styled.TextContent>
-              </Styled.Pool>
-            ) : null
-          )}
-        </Styled.Pools>
-        <Styled.ShowMore href="#completed_pools">
-          <p>Show More</p>
-          <Styled.IconWrapper>
-            <ChevronRightIcon />
-          </Styled.IconWrapper>
-        </Styled.ShowMore>
-      </Styled.Content>
-    </Styled.Container>
-  );
+	const modalClick = (e: any) => {
+		setProjectData(e);
+		toggleModal((prev) => !prev);
+	};
+
+	return (
+		<Styled.Container>
+			<Styled.Content>
+				<Styled.Header>
+					<Heading02 id="upcoming_pools">Upcoming Pools</Heading02>
+					<Styled.Filters>
+						<Styled.Filter style={{ backgroundColor: "#A259FF" }}>
+							Public
+						</Styled.Filter>
+						<Styled.Filter style={{ border: "1px solid #000", color: "#000" }}>
+							Private
+						</Styled.Filter>
+						<Styled.Filter style={{ backgroundColor: "#FF004C" }}>
+							0 - 5 days
+						</Styled.Filter>
+					</Styled.Filters>
+				</Styled.Header>
+
+				{isVisible && (
+					<ModalInvoice
+						isVisible={isVisible}
+						toggleModal={() => toggleModal((prev) => !prev)}
+						projectData={projectData}
+					/>
+				)}
+
+				<Styled.Pools>
+					{bigArr.status.map((e: any) =>
+						e ? (
+							<Tilt
+								children={
+									<Styled.Pool onClick={() => modalClick(e)}>
+										<Styled.ImageWrapper>
+											<Styled.PoolImage src={e.Picture} alt="Eclipse" />
+										</Styled.ImageWrapper>
+										<Styled.TextContent>
+											<Styled.PoolHeader>
+												<Styled.PoolHeading>{e.Title}</Styled.PoolHeading>
+												<Styled.Tags>
+													<Styled.Tag style={{ backgroundColor: "#A259FF" }}>
+														Public
+													</Styled.Tag>
+													<Styled.Tag style={{ backgroundColor: "#FF004D" }}>
+														{e.EndDate === 10
+															? "none"
+															: //@ts-ignore
+															  dayjs().to(dayjs(Number(e.EndDate)))}
+														{/* dayjs(Number(e.EndDate)).format('MM/YYYY') */}
+													</Styled.Tag>
+												</Styled.Tags>
+											</Styled.PoolHeader>
+											<Styled.PoolText>
+												{e.Description.length > 200
+													? e.Description.slice(0, 200) + "..."
+													: e.Description}
+											</Styled.PoolText>
+											<Styled.Allocation>
+												<Styled.AllocationAmount>
+													<Styled.AllocationText>
+														Rised (TON)
+													</Styled.AllocationText>
+													<Styled.AllocationHeading>
+														{e.Rised}
+													</Styled.AllocationHeading>
+												</Styled.AllocationAmount>
+												<Styled.AllocationAmount>
+													<Styled.AllocationText>
+														Maximum (TON)
+													</Styled.AllocationText>
+													<Styled.AllocationHeading>
+														{e.Max}
+													</Styled.AllocationHeading>
+												</Styled.AllocationAmount>
+											</Styled.Allocation>
+										</Styled.TextContent>
+									</Styled.Pool>
+								}
+							/>
+						) : null
+					)}
+				</Styled.Pools>
+				<Styled.ShowMore href="#completed_pools">
+					<p>Show More</p>
+					<Styled.IconWrapper>
+						<ChevronRightIcon />
+					</Styled.IconWrapper>
+				</Styled.ShowMore>
+			</Styled.Content>
+		</Styled.Container>
+	);
 };
 
 export default UpcomingPoolsBlock;
