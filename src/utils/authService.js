dc1f93fc45c04b388380ce76a5e8c7208c1abbc5
“murluki” <timofeli.work@gmail.com>
2023-10-31 19:33:21 +0100

    added .env variables for backend

--- src/utils/authService.js
+++ src/utils/authService.js
@@ -1,42 +1,43 @@
 export const SignIn = async (username, password) => {
-    try {
-      const response = await fetch('YOUR_BACKEND_URL/signin', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password }),
+  const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000'; //  default value
+  try {
+      const response = await fetch(`${backendUrl}/signin`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ username, password }),
       });
       if (response.ok) {
-        const userData = await response.json();
-        return userData;
+          const userData = await response.json();
+          return userData;
       } else {
-        console.error('Sign in failed');
-        return null;
+          console.error('Sign in failed');
+          return null;
       }
-    } catch (err) {
+  } catch (err) {
       console.error('Error during sign in', err);
       return null;
-    }
-  };
-  
-  export const SignUp = async (username, password, email) => {
-    try {
-      const response = await fetch('YOUR_BACKEND_URL/signup', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password, email }),
+  }
+};
+
+export const SignUp = async (username, password, email) => {
+  const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000'; // default value
+  try {
+      const response = await fetch(`${backendUrl}/signup`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ username, password, email }),
       });
       if (response.ok) {
-        const userData = await response.json();
-        return userData;
+          const userData = await response.json();
+          return userData;
       } else {
-        console.error('Sign up failed');
-        const errorData = await response.json();
-        console.error(errorData);
-        return null;
+          console.error('Sign up failed');
+          const errorData = await response.json();
+          console.error(errorData);
+          return null;
       }
-    } catch (err) {
+  } catch (err) {
       console.error('Error during sign up', err);
       return null;
-    }
-  };
-  
+  }
+};
