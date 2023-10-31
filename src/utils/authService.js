791d321ec9d2896dc87977a2bc4f1e1225c03bfe
“murluki” <timofeli.work@gmail.com>
2023-10-31 19:40:10 +0100

    added jwt handling to auth

--- src/utils/authService.js
+++ src/utils/authService.js
@@ -1,43 +1,85 @@
-export const SignIn = async (username, password) => {
-  const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000'; //  default value
-  try {
+import { useState } from 'react';
+
+export const useAuth = () => {
+  const [user, setUser] = useState(null);
+
+  const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000'; // default value
+  
+  const SignIn = async (username, password) => {
+    try {
       const response = await fetch(`${backendUrl}/signin`, {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ username, password }),
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
       });
+
       if (response.ok) {
-          const userData = await response.json();
-          return userData;
+        const userData = await response.json();
+        localStorage.setItem('token', userData.token);
+        setUser(userData.user); // Assuming the response includes a user object
+        return userData;
       } else {
-          console.error('Sign in failed');
-          return null;
+        console.error('Sign in failed');
+        return null;
       }
-  } catch (err) {
+    } catch (err) {
       console.error('Error during sign in', err);
       return null;
-  }
-};
+    }
+  };
 
-export const SignUp = async (username, password, email) => {
-  const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000'; // default value
-  try {
+  const SignUp = async (username, password, email) => {
+    try {
       const response = await fetch(`${backendUrl}/signup`, {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ username, password, email }),
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password, email }),
       });
+
       if (response.ok) {
-          const userData = await response.json();
-          return userData;
+        const userData = await response.json();
+        localStorage.setItem('token', userData.token);
+        setUser(userData.user); // Assuming the response includes a user object
+        return userData;
       } else {
-          console.error('Sign up failed');
-          const errorData = await response.json();
-          console.error(errorData);
-          return null;
+        console.error('Sign up failed');
+        const errorData = await response.json();
+        console.error(errorData);
+        return null;
       }
-  } catch (err) {
+    } catch (err) {
       console.error('Error during sign up', err);
       return null;
-  }
+    }
+  };
+
+  const signOut = () => {
+    localStorage.removeItem('token');
+    setUser(null);
+  };
+
+  const fetchAuthenticated = async (url, options = {}) => {
+    const token = localStorage.getItem('token');
+    const response = await fetch(url, {
+      ...options,
+      headers: {
+        ...options.headers,
+        'Authorization': `Bearer ${token}`
+      },
+    });
+
+    if (response.status === 401) {
+      signOut(); // Optional: redirect to login or show a message
+    }
+
+    return response;
+  };
+
+  return {
+    user,
+    SignIn,
+    SignUp,
+    signOut,
+    fetchAuthenticated,
+  };
 };
